295813b68d285ae255927647e84a9fa34c547131
Arthur K <arthurkuhn@outlook.com>
2016-04-30 17:16:53 +1000

    GUI mods

--- GUI/QCustomWidget.py
+++ GUI/QCustomWidget.py
@@ -10,25 +10,48 @@ class QCustomQWidget (QWidget):
     def __init__ (self, parent = None):
         super(QCustomQWidget, self).__init__(parent)
         self.textQVBoxLayout = QVBoxLayout()
-        self.textUpQLabel    = QLabel()
-        self.textDownQLabel  = QLabel()
-        self.textQVBoxLayout.addWidget(self.textUpQLabel)
-        self.textQVBoxLayout.addWidget(self.textDownQLabel)
+        self.ifQHBoxLayout = QHBoxLayout()
+        self.thenQHBoxLayout = QHBoxLayout()
+        
+        self.ifTextQLabel = QLabel('If',self)
+        self.ifTextQComboBox    = QComboBox(self)
+        
+        self.equalTextQLabel = QLabel(' = ',self)
+        self.equalTextQComboBox    = QComboBox(self)
+        
+        self.thenTextQLabel = QLabel('Then ', self)
+        self.thenTextQComboBox    = QComboBox(self)
+        
+        
+        self.ifQHBoxLayout.addWidget(self.ifTextQLabel)
+        self.ifQHBoxLayout.addWidget(self.ifTextQComboBox)
+        self.ifQHBoxLayout.addWidget(self.equalTextQLabel)
+        self.ifQHBoxLayout.addWidget(self.equalTextQComboBox)
+        
+        self.thenQHBoxLayout.addWidget(self.thenTextQLabel)
+        self.thenQHBoxLayout.addWidget(self.thenTextQComboBox)
+        
+        self.textQVBoxLayout.addLayout(self.ifQHBoxLayout,0)
+        self.textQVBoxLayout.addLayout(self.thenQHBoxLayout,1)
         self.allQHBoxLayout  = QHBoxLayout()
         self.iconQLabel      = QLabel()
         self.allQHBoxLayout.addWidget(self.iconQLabel, 0)
         self.allQHBoxLayout.addLayout(self.textQVBoxLayout, 1)
         self.setLayout(self.allQHBoxLayout)
         # setStyleSheet
-        self.textUpQLabel.setStyleSheet('''
+        self.ifTextQComboBox.setStyleSheet('''
             color: rgb(0, 0, 255);
         ''')
-        self.textDownQLabel.setStyleSheet('''
+        self.equalTextQComboBox.setStyleSheet('''
+            color: rgb(0, 0, 255);
+        ''')
+        self.thenTextQComboBox.setStyleSheet('''
             color: rgb(255, 0, 0);
         ''')
 
-    def setTextUp (self, text):
-        self.textUpQLabel.setText(text)
-
-    def setTextDown (self, text):
-        self.textDownQLabel.setText(text)
+    def setIfConditions (self, list_of_strings):
+        self.ifTextQComboBox.addItems(list_of_strings)
+    def setEqualConditions (self, list_of_strings):
+        self.equalTextQComboBox.addItems(list_of_strings)
+    def setThenConditions (self, list_of_strings):
+        self.thenTextQComboBox.addItems(list_of_strings)
